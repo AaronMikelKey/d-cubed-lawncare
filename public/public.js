cbc0a7d97cf5dcd42f7b6c354a8f3bacf8d81c6d
Aaron Key <aaronmikelkey@gmail.com>
2022-09-08 19:55:23 -0400

    Fixed error from merge

--- public/public.js
+++ public/public.js
@@ -1,17 +1,6 @@
-<<<<<<< HEAD
-(function($){
-    $(function(){
-  
-      $('.sidenav').sidenav();
-      $('.parallax').parallax();
-  
-    }); // end of document ready
-  })(jQuery); // end of jQuery name space
-=======
 (function ($) {
   $(function () {
     $(".sidenav").sidenav();
     $(".parallax").parallax();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
->>>>>>> 298d2fdf604454413c79ecaf7c9461023bab76c4
