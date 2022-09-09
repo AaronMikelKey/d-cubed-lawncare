6b9b1b64cf86ab9834ae78d35aff70355f8e3ce1
RKH1988 <regettaspearman@gmail.com>
2022-09-08 21:09:46 -0400

    added models for appointments and reviews

---
+++ models/appointments.js
@@ -0,0 +1,84 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// // create our Appointment model
+// class Appointments extends Model {
+//   static upvote(body, models) {
+//     return models.Vote.create({
+//       user_id: body.user_id,
+//       post_id: body.post_id
+//     }).then(() => {
+//       return Post.findOne({
+//         where: {
+//           id: body.post_id
+//         },
+//         attributes: [
+//           'id',
+//           'post_url',
+//           'title',
+//           'created_at',
+//           [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+//         ],
+//         include: [
+//           {
+//             model: models.Comment,
+//             attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+//             include: {
+//               model: models.User,
+//               attributes: ['username']
+//             }
+//           }
+//         ]
+//       });
+//     });
+//   }
+// }
+
+// create fields/columns for Appointment model
+Appointments.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    service: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    appointment_url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isURL: true
+      }
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    },
+    schedule_id: {
+        type: DataTypes.INTEGER,
+      references: {
+        model: 'schedule',
+        key: 'id'
+      }
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'appointments'
+  }
+);
+
+module.exports = Post;
