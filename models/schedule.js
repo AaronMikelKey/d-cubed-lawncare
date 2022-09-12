9f627a3a9dd38bbaf49767fd2497e0cf5a43e868
RKH1988 <regettaspearman@gmail.com>
2022-09-12 16:52:46 -0400

    added line to appointments and scheduel

--- models/schedule.js
+++ models/schedule.js
@@ -1,6 +1,8 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 
+class Schedule extends Model {}
+
 // create fields/columns for Schedule model
 Schedule.init(
   {
