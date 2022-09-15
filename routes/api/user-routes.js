363b93487ca5c379263967ea58568cf1c684d374
Aaron Key <aaronmikelkey@gmail.com>
2022-09-15 19:43:50 -0400

    fix route

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -102,30 +102,27 @@ router.post("/login", (req, res, next) => {
 });
 
 router.post("/dashboard", (req, res) => {
-  let data = {};
+  let data = {
+    username: "",
+    admin: null,
+    schedule: "",
+  };
 
   Schedule.findAll()
-    .then((data) => {
-      console.log("schedule" + data);
-      return data;
+    .then(() => {
+      data.schedule = data;
     })
-    .catch((err) => console.log(err))
     .then(
       User.findOne({
         where: {
           username: req.body.username,
         },
+      }).then((dbUserData) => {
+        (data.username = dbUserData.username), (data.admin = dbUserData.admin);
+        return data;
       })
     )
-    .then((dbUserData) => {
-      (data.username = dbUserData.username), (data.admin = dbUserData.admin);
-    })
-    .then((scheduleData) => {
-      console.log("schedule2" + scheduleData);
-      data.schedule = scheduleData;
-    })
     .then(() => {
-      data = JSON.stringify(data);
       res.json(data);
     });
 });
