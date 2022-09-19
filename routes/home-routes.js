10479dd5180963303c05c74872641bc83e0c3a4b
Aaron Key <aaronmikelkey@gmail.com>
2022-09-18 23:45:54 -0400

    Fixed auth on login

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -1,41 +1,79 @@
 const router = require("express").Router();
 const fetch = require("node-fetch");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-router.get("/", (req, res) => {
-  console.log(req.cookies);
-  const data = {
-    username: req.session.username,
-    expires: req.session._expires,
+// JWT auth function
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token == null) return res.status(401);
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.status(403).json({ err: err });
+    req.user = user;
+
+    next();
+  });
+};
+
+router.get("/", (req, res, next) => {
+  if (!req.cookies.jwtAuth) {
+    console.log("cookie error");
+    return res.render("welcome");
+  }
+
+  const token = req.cookies.jwtAuth;
+  const tokenDecodablePart = token.data.split(".")[1];
+  const decoded = {
+    data: {
+      username: Buffer.from(tokenDecodablePart, "base64").toString(),
+    },
   };
-  res.render("welcome", data);
+  console.log(decoded);
+
+  const username = jwt.verify(req.cookies.jwtAuth.data, process.env.JWT_SECRET);
+  res.render("welcome", decoded);
 });
 
 router.get("/login", (req, res) => {
-  if (req.session.username) {
+  if (req.cookies.jwtAuth) {
     res.redirect("/");
   }
   res.render("login");
 });
 
 router.post("/login", async (req, res) => {
-  const token = await fetch("https://d-cubed.herokuapp.com/api/login", {
+  fetch("https://d-cubed.herokuapp.com/api/login", {
     method: "POST",
-    cache: "no-cache",
-    credentials: "include",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
       username: req.body.username,
-      password: bcrypt.hash(req.body.password, 10),
+      password: req.body.password,
     }),
-  });
-  res.cookie("jwtAuth", token, {
-    expires: new Date(Date.now() + 3600000),
-    secure: false,
-  });
-  res.redirect("/");
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      res.cookie(
+        "jwtAuth",
+        { data: data },
+        {
+          expires: new Date(Date.now() + 3600000),
+          secure: false,
+        }
+      );
+
+      res.redirect("/");
+      if (data.error) {
+        console.log(data.error);
+        return res.render("login", data.error);
+      }
+    });
 });
 
 router.get("/signup", (req, res) => {
@@ -57,8 +95,9 @@ const getUser = async (url, data) => {
   return response;
 };
 
-router.get("/dashboard", async (req, res) => {
-  const username = req.session.username;
+router.get("/dashboard", authenticateToken, async (req, res) => {
+  const username = req.user;
+  console.log(username);
   getUser("https://d-cubed.herokuapp.com/api/dashboard", {
     username: username,
   }).then((data) => {
